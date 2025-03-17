c98148a64cbd4964ae06cb0b1e3d7252b58ec962
SANJITH2002 <167007847+SANJITH2002@users.noreply.github.com>
2025-03-17 13:26:31 +0530

    Update DemoApplication.java

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-        
-	int x = 10;
+        int x = 10;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
