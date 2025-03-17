8fdcda08ee8bc631e918e30b611b274624db8a90
SANJITHCSE20 <sanjith.cs20@bitsathy.ac.in>
2025-03-17 13:38:39 +0530

    first message

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-        char r = 'a';
+        String name = "mike";
+	int x = 10;
+	boolean isPresent = false;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
